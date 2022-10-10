e6abc1f01b40cf8fee971cca3567c1aedc951504
victorbern <victorbernardo96@gmail.com>
2022-10-10 16:42:47 -0300

    Teste de branch

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config({path:"variaveis.env"});
+require("dotenv").config({ path: "variaveis.env" });
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-let dados = require('./database')
+let dados = require("./database");
 // dados.inserirDados();
 const app = express();
 
 const routes = require("./api/routes/routes");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api", routes);
-app.get('/', (req, res) => res.send("Aplicação Rodando!"))
-
+app.get("/", (req, res) => res.send("Aplicação Rodando!"));
+app.use();
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
